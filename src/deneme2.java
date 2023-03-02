08e2ec2e969d2fa9d576778b3ea6d315744d7d03
Sezer Ercan <sezercan0617@gmail.com>
2023-03-03 02:12:44 +0300

    v8

--- src/deneme2.java
+++ src/deneme2.java
@@ -1,6 +1,11 @@
 public class deneme2 {
     public static void main(String[] args) {
         System.out.println("bu ikinci denememiz");
+        System.out.println("bu ikinci denememiz");
+        System.out.println("bu ikinci denememiz");
+        System.out.println("bu ikinci denememiz");
+        System.out.println("bu ikinci denememiz");
+        System.out.println("bu ikinci denememiz");
 
     }
 }
